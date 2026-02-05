6953c1d2d6a4d8798035d128d685012201df5fb7
NicolaiBruunLarsen <nico674d@gmail.com>
2026-02-05 17:20:29 +0100

    update

--- exercises/math/sfuns.cc
+++ exercises/math/sfuns.cc
@@ -1,45 +1,27 @@
-#include <cmath>
-#include <numbers>
+#include<cmath>
+#include<numbers> // c++20
 #include"sfuns.h"
+#include<limits>
 
 
+namespace sfuns{
+constexpr double PI = std::numbers::pi; // c++20
+constexpr double NaN = std::numeric_limits<double>::quiet_NaN();
+// constexpr double PI = 3.14159265358979323846; // pre- c++20
 
-namespace sfuns {
 
-constexpr double PI = std::numbers::pi;
-
-
-
-//  for task 2
 double fgamma(double x){
-  if(x<0) return PI/std::sin(PI*x)/fgamma(1-x);
-	if(x<9) return fgamma(x+1)/x;
+	if(x<0)return PI/std::sin(PI*x)/fgamma(1-x);
+	if(x<9)return fgamma(x+1)/x;
 	double lnfgamma=x*std::log(x+1/(12*x-1/x/10))-x+std::log(2*PI/x)/2;
 	return std::exp(lnfgamma);
-	}
-
-
-// for task 3
-#include <limits>
-
-
-static double stirling_lngamma_core(double x) {
-  const double t = x + 1.0 / (12.0 * x - 1.0 / (10.0 * x));
-  const double lnf = x * std::log(t) - x + 0.5 * std::log(2.0 * PI / x);
-  return lnf;
 }
 
-
-double lngamma(double x) {
-  constexpr double NaN = std::numeric_limits<double>::quiet_NaN();
-
-  if (x <= 0.0) return NaN;
-
-  if (x < 9.0) {
-    return lngamma(x + 1.0) - std::log(x);
-  }
-
-  return stirling_lngamma_core(x);
+double lngamma(double x){
+	if(x <= 0) return NaN;
+	if(x < 9) return lngamma(x+1) - std::log(x);
+	double lnfgamma=x*std::log(x+1/(12*x-1/(10*x)))-x+std::log(2*PI/x)/2;
+    return lnfgamma;
 }
 
 }
