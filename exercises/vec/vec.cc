4b93f47939bfb14fd87ff1d7e664bc1e457cbe24
NicolaiBruunLarsen <nico674d@gmail.com>
2026-03-05 15:49:17 +0100

    vec

---
+++ exercises/vec/vec.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "vec.h"
+
+
+
+int main() {
+
+    
+void vec::print(std::string s) const {
+    std::cout << s << x << " " << y << " " << z << std::endl;
+    }
+
+
+
+
+    return 0;
+}
