eb51b308cf0f9ae44342e9bad772f37d0021cba1
NicolaiBruunLarsen <nico674d@gmail.com>
2026-06-22 16:21:11 +0200

    small fix

--- homeworks/roots/main.cc
+++ homeworks/roots/main.cc
@@ -26,7 +26,7 @@ pp::vector himmel_grad(pp::vector x){
 }
 
 double hydrogen_endpoint(double E, double rmin, double rmax, double acc, double eps){
-	std::function<pp::vector(double,pp::vector)> F = [=](double r, pp::vector y){
+	std::function<pp::vector(double,pp::vector)> F = [=](double r, pp::vector y){ // c++ lambda function 
 		return pp::vector{y[1],-2*(E+1/r)*y[0]};
 	};
 	pp::vector y0{rmin-rmin*rmin,1-2*rmin};
@@ -35,7 +35,7 @@ double hydrogen_endpoint(double E, double rmin, double rmax, double acc, double 
 }
 
 double find_energy(double rmin, double rmax, double acc, double eps){
-	std::function<pp::vector(pp::vector)> M = [=](pp::vector x){
+	std::function<pp::vector(pp::vector)> M = [=](pp::vector x){ 
 		return pp::vector{hydrogen_endpoint(x[0],rmin,rmax,acc,eps)};
 	};
 	pp::vector E = pp::newton(M,pp::vector{-0.7},1e-5);
@@ -48,7 +48,7 @@ void write_wavefunction(double E, double rmin, double rmax, double acc, double e
 	};
 	pp::vector y0{rmin-rmin*rmin,1-2*rmin};
 	auto [rs,ys] = pp::driver(F,rmin,rmax,y0,0.05,acc,eps);
-	std::ofstream out("out.wavefunction.txt");
+	std::ofstream out("out_wavefunction.txt");
 	for(size_t i=0;i<rs.size();i++){
 		double r=rs[i];
 		out << r << " " << ys[i][0] << " " << r*std::exp(-r) << "\n";
@@ -58,19 +58,19 @@ void write_wavefunction(double E, double rmin, double rmax, double acc, double e
 void write_convergence(){
 	double rmin=1e-3;
 	double rmax=8;
-	std::ofstream out_rmax("out.convergence.rmax.txt");
+	std::ofstream out_rmax("out_convergence_rmax.txt");
 	for(double R : {4.0,5.0,6.0,7.0,8.0,10.0,12.0}){
 		double E=find_energy(rmin,R,1e-6,1e-6);
 		out_rmax << R << " " << E << " " << std::abs(E+0.5) << "\n";
 	}
 
-	std::ofstream out_rmin("out.convergence.rmin.txt");
+	std::ofstream out_rmin("out_convergence_rmin.txt");
 	for(double r0 : {1e-1,5e-2,2e-2,1e-2,5e-3,1e-3}){
 		double E=find_energy(r0,rmax,1e-6,1e-6);
 		out_rmin << r0 << " " << E << " " << std::abs(E+0.5) << "\n";
 	}
 
-	std::ofstream out_acc("out.convergence.acc.txt");
+	std::ofstream out_acc("out_convergence_acc.txt");
 	for(double acc : {1e-3,1e-4,1e-5,1e-6,1e-7}){
 		double E=find_energy(rmin,rmax,acc,acc);
 		out_acc << acc << " " << E << " " << std::abs(E+0.5) << "\n";
